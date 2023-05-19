a87a17f3fb7912012b6a1d5c8dd98b19dcedf29c
Serhii_Kolisnyk <Serhii.Kolisnyk@clarivate.com>
2023-05-19 04:50:21 +0200

    print line change

--- cmd/displayPwdHash/main.go
+++ cmd/displayPwdHash/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+
 	"github.com/Serhii1Epam/lets-go-chat/pkg/hasher"
 )
 
 func main() {
-	fmt.Printf("displayPwdHash =>%v, Hash => %s", os.Args[1:], HashPassword(os.Args[1:]))
+	fmt.Printf("displayPwdHash: Password => %v, Hash => %s", os.Args[1:], hasher.HashPassword(os.Args[1:]))
 }
