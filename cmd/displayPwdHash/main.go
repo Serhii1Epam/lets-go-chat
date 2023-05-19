0e3a6fb2068d2e6ee9c0b1528b2d8f3f33bc8f97
Serhii_Kolisnyk <Serhii.Kolisnyk@clarivate.com>
2023-05-19 09:50:07 +0200

    cmd check for empty parameter

--- cmd/displayPwdHash/main.go
+++ cmd/displayPwdHash/main.go
@@ -6,10 +6,23 @@ package main
 import (
 	"fmt"
 	"os"
-
 	"github.com/Serhii1Epam/lets-go-chat/pkg/hasher"
 )
 
 func main() {
-	fmt.Printf("displayPwdHash: Password => %v, Hash => %s", os.Args[1:], hasher.HashPassword(os.Args[1:]))
+	var arg string
+
+	if numArgs := len(os.Args); numArgs > 1 {
+		arg = os.Args[1]
+	} else {
+		fmt.Println("Hashing errors: epmpty password entered")
+		return
+	}
+
+	hash, err := hasher.HashPassword(arg)
+	if err != nil {
+		fmt.Println("Hashing errors: arg => ", arg)
+	}
+
+	fmt.Printf("displayPwdHash: Password => %s, Hash => %s", arg, hash)
 }
